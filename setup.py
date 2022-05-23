e151b6822adff496124a60d4fafa31c8b1a40ac1
coolnicecool <70819161+coolnicecool@users.noreply.github.com>
2022-05-22 20:23:51 -0500

    Update setup.py
    update setup

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 setup(
-    name="pykernel",
-    version="0.0.3",
+    name="editor",
+    version="0.1.0",
     description="Python & text Windows CMD Editior written with only native libaries",
-    py_modules=["pykernel"],
+    py_modules=["editor","TIME"],
     package_dir={"":"code"},
     url="https://github.com/coolnicecool/Pykernel",
     author="Raleigh Priour"
